ffe3d7f0aa19cce1f70d1e67cc3955df1cfa1374
İsmail <kaygisizismail24@gmail.com>
2023-09-09 18:39:18 +0300

    UpdateLangugageCommand Added and Tested

---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Features/Languages/Commands/Update/UpdateLanguageCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TransportathonHackathon.Application.Features.Languages.Commands.Update
+{
+    public class UpdateLanguageCommand : IRequest<UpdatedLanguageResponse>
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string GloballyName { get; set; }
+        public string LocallyName { get; set; }
+    }
+}
