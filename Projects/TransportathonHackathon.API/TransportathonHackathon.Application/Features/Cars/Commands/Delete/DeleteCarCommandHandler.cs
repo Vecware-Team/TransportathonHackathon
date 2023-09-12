29dd91f0e177631b03ae85f3c46f5b444bd8d393
İsmail <kaygisizismail24@gmail.com>
2023-09-12 18:50:37 +0300

    Features/Cars Completed

---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Features/Cars/Commands/Delete/DeleteCarCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Core.CrossCuttingConcerns.Exceptions.ExceptionTypes;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TransportathonHackathon.Application.Repositories;
+using TransportathonHackathon.Domain.Entities;
+
+namespace TransportathonHackathon.Application.Features.Cars.Commands.Delete
+{
+    public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand, DeletedCarResponse>
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+        private readonly ICarRepository _carRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteCarCommandHandler(IVehicleRepository vehicleRepository, ICarRepository carRepository, IMapper mapper)
+        {
+            _vehicleRepository = vehicleRepository;
+            _carRepository = carRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<DeletedCarResponse> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+        {
+            Car? car = await _carRepository.GetAsync(
+                e => e.VehicleId == request.VehicleId,
+                include: e => e.Include(e => e.Vehicle).Include(e => e.Vehicle.Company).Include(e => e.Vehicle.Driver)
+            );
+            if (car is null)
+                throw new NotFoundException("Car not found");
+
+            Vehicle vehicle = _mapper.Map<Vehicle>(car.Vehicle);
+            await _vehicleRepository.DeleteAsync(car.Vehicle);
+            car.Vehicle = vehicle;
+
+            DeletedCarResponse response = _mapper.Map<DeletedCarResponse>(car);
+            return response;
+        }
+    }
+}
---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Features/Cars/Commands/Update/UpdateCarCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Core.CrossCuttingConcerns.Exceptions.ExceptionTypes;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TransportathonHackathon.Application.Repositories;
+using TransportathonHackathon.Domain.Entities;
+
+namespace TransportathonHackathon.Application.Features.Cars.Commands.Update
+{
+    public class UpdateCarCommandHandler : IRequestHandler<UpdateCarCommand, UpdatedCarResponse>
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateCarCommandHandler(ICarRepository carRepository, IMapper mapper)
+        {
+            _carRepository = carRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UpdatedCarResponse> Handle(UpdateCarCommand request, CancellationToken cancellationToken)
+        {
+            Car? car = await _carRepository.GetAsync(
+                e => e.VehicleId == request.VehicleId,
+                include: e => e.Include(e => e.Vehicle).Include(e => e.Vehicle.Company).Include(e => e.Vehicle.Driver)
+            );
+            if (car is null)
+                throw new NotFoundException("Car not found");
+
+            car.Vehicle.CompanyId = request.CompanyId;
+            car.Vehicle.DriverId = request.DriverId;
+            car.Vehicle.UpdatedDate = DateTime.UtcNow;
+            car.Brand = request.Brand;
+            car.Model = request.Model;
+            car.ModelYear = request.ModelYear;
+            car.UpdatedDate = DateTime.Now;
+
+            await _carRepository.SaveChangesAsync();
+            car = await _carRepository.GetAsync(
+               e => e.VehicleId == request.VehicleId,
+               include: e => e.Include(e => e.Vehicle).Include(e => e.Vehicle.Company).Include(e => e.Vehicle.Driver)
+           );
+
+            UpdatedCarResponse response = _mapper.Map<UpdatedCarResponse>(car);
+            return response;
+        }
+    }
+}
