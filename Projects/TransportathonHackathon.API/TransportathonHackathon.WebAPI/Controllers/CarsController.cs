1196a11dd52e3b3e9754be1d1db41443f3169011
İsmail <kaygisizismail24@gmail.com>
2023-09-12 22:48:26 +0300

    Controllers Added

---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,51 @@
+﻿using Core.API.Controllers;
+using Core.Persistence.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using TransportathonHackathon.Application.Features.Cars.Commands.Create;
+using TransportathonHackathon.Application.Features.Cars.Commands.Delete;
+using TransportathonHackathon.Application.Features.Cars.Commands.Update;
+using TransportathonHackathon.Application.Features.Cars.Queries.GetById;
+using TransportathonHackathon.Application.Features.Cars.Queries.GetList;
+
+namespace TransportathonHackathon.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCarCommand command)
+        {
+            CreatedCarResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] DeleteCarCommand command)
+        {
+            DeletedCarResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateCarCommand command)
+        {
+            UpdatedCarResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet("{VehicleId}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdCarQuery command)
+        {
+            GetByIdCarResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] GetListCarQuery command)
+        {
+            IPaginate<GetListCarResponse> response = await Mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.WebAPI/Controllers/TransportRequestsController.cs
@@ -0,0 +1,51 @@
+﻿using Core.API.Controllers;
+using Core.Persistence.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using TransportathonHackathon.Application.Features.TransportRequests.Commands.Create;
+using TransportathonHackathon.Application.Features.TransportRequests.Commands.Delete;
+using TransportathonHackathon.Application.Features.TransportRequests.Commands.Update;
+using TransportathonHackathon.Application.Features.TransportRequests.Queries.GetById;
+using TransportathonHackathon.Application.Features.TransportRequests.Queries.GetList;
+
+namespace TransportathonHackathon.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TransportRequestsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTransportRequestCommand command)
+        {
+            CreatedTransportRequestResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] DeleteTransportRequestCommand command)
+        {
+            DeletedTransportRequestResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateTransportRequestCommand command)
+        {
+            UpdatedTransportRequestResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdTransportRequestQuery command)
+        {
+            GetByIdTransportRequestResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] GetListTransportRequestQuery command)
+        {
+            IPaginate<GetListTransportRequestResponse> response = await Mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
