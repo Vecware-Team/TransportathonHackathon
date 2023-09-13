0ce891a4df4c006d921ab10f50e1f90775e34efc
İsmail <kaygisizismail24@gmail.com>
2023-09-13 16:35:47 +0300

    Update BusinessRulesExtensions.cs

--- Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Extensions/BusinessRulesExtensions.cs
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Extensions/BusinessRulesExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using Core.Application.Rules;
 using Core.CrossCuttingConcerns.Exceptions.ExceptionTypes;
 using Core.CrossCuttingConcerns.IoC;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
 using TransportathonHackathon.Domain.Entities.Identity;
 
 namespace TransportathonHackathon.Application.Extensions
@@ -16,6 +18,29 @@ namespace TransportathonHackathon.Application.Extensions
 
             if (user is not null)
                 throw new BusinessException("User already exists");
+        }
+
+        public static async Task CheckIfUserIsNotOwnerTheObject(this BaseBusinessRules rules, Guid userId)
+        {
+            IHttpContextAccessor httpContextAccessor = ServiceTool.ServiceProvider.GetRequiredService<IHttpContextAccessor>();
+            UserManager<AppUser> userManager = ServiceTool.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+            AppUser? user = await userManager.FindByIdAsync(userId.ToString());
+
+            if (user is null)
+                throw new BusinessException("User is not exists");
+
+            if (httpContextAccessor.HttpContext.User is null || httpContextAccessor.HttpContext.User?.Claims is null)
+                throw new UnauthorizedException();
+
+            if (httpContextAccessor.HttpContext.User.Claims.SingleOrDefault(e=>e.Type == ClaimTypes.NameIdentifier) is null)
+                throw new UnauthorizedException();
+
+            httpContextAccessor.HttpContext.User.Claims.ToList().ForEach(claim =>
+            {
+                if(claim.Type == ClaimTypes.NameIdentifier)
+                    if(user.Id.ToString() != claim.Value)
+                        throw new AuthorizationDeniedException();
+            });
         }
     }
 }
