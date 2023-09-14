14040e1d71187bcd0691df8c7c204e4fc35f9e84
İsmail <kaygisizismail24@gmail.com>
2023-09-14 14:14:07 +0300

    Migrations Updated

---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Persistence/Migrations/20230914111333_Mig_9.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportathonHackathon.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig_9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "ModelYear",
+                table: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "PickupTrucks");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "PickupTrucks");
+
+            migrationBuilder.DropColumn(
+                name: "ModelYear",
+                table: "PickupTrucks");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "ModelYear",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModelYear",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "ModelYear",
+                table: "Vehicles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Trucks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Trucks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModelYear",
+                table: "Trucks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "PickupTrucks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "PickupTrucks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModelYear",
+                table: "PickupTrucks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModelYear",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
