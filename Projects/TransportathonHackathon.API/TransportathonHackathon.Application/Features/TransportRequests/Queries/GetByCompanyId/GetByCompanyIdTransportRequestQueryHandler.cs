81c2de0976f2c8603a94363ca4a188e914ce5643
İsmail <kaygisizismail24@gmail.com>
2023-09-16 01:54:08 +0300

    New Property Added to Responses

--- Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Features/TransportRequests/Queries/GetByCompanyId/GetByCompanyIdTransportRequestQueryHandler.cs
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Application/Features/TransportRequests/Queries/GetByCompanyId/GetByCompanyIdTransportRequestQueryHandler.cs
@@ -21,7 +21,7 @@ namespace TransportathonHackathon.Application.Features.TransportRequests.Queries
         {
             IList<TransportRequest> transportRequest = await _transportRequestRepository.GetListAsync(
                 e => e.CompanyId == request.CompanyId,
-                include: e => e.Include(e => e.Company).Include(e => e.Customer).Include(e => e.TransportType)
+                include: e => e.Include(e => e.Company).Include(e => e.Customer).Include(e => e.TransportType).Include(e => e.PaymentRequest)
             );
 
             List<GetByCompanyIdTransportRequestResponse> response = _mapper.Map<List<GetByCompanyIdTransportRequestResponse>>(transportRequest.ToList());
