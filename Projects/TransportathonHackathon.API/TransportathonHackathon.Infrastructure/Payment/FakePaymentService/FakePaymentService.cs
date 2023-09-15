7ab3d8cdd0a7f3b94b7eb256801c6bcc85f8875b
İsmail <kaygisizismail24@gmail.com>
2023-09-15 15:18:20 +0300

    IPaymentService and FakePaymentService Added to Infrastructure. PaymentController Added

---
+++ Projects/TransportathonHackathon.API/TransportathonHackathon.Infrastructure/Payment/FakePaymentService/FakePaymentService.cs
@@ -0,0 +1,10 @@
+﻿namespace TransportathonHackathon.Infrastructure.Payment.FakePaymentService
+{
+    public class FakePaymentService : IPaymentService
+    {
+        public async Task<bool> Payment(PaymentObject paymentObject)
+        {
+            return true;
+        }
+    }
+}
